869a32d59a96f37c39c8681121a4b63e9132d824
Oliver Bähler <oliverbaehler@hotmail.com>
2025-04-29 16:03:24 +0200

    chore: migrate to capsule addons

--- api/v1alpha1/sopssecret_types.go
+++ api/v1alpha1/sopssecret_types.go
@@ -1,6 +1,17 @@
 /*
-Copyright 2024 Peak Scale
-SPDX-License-Identifier: Apache-2.0
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 package v1alpha1
@@ -85,7 +96,7 @@ type SopsSecret struct {
 	Sops   *api.SopsMetadata `json:"sops,omitempty"`
 }
 
-//+kubebuilder:object:root=true
+// +kubebuilder:object:root=true
 
 // SopsSecretList contains a list of SopsSecret.
 type SopsSecretList struct {
